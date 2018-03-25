5bea3a11470c951c29f9d186c92f18f71bd483e3
Schneizel <744587537@qq.com>
2018-03-25 20:22:45 +0800

    Create 二叉树的景象.cpp

---
+++ CodingInterview/二叉树的景象.cpp
@@ -0,0 +1,42 @@
+/*
+时间限制：1秒 空间限制：32768K 热度指数：124871
+题目描述
+操作给定的二叉树，将其变换为源二叉树的镜像。
+输入描述:
+二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+    	11 9 7  5
+
+*/
+
+
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        if(!pRoot) return;
+        TreeNode* temp;
+        temp = pRoot->left;
+        pRoot->left = pRoot->right;
+        pRoot->right = temp;
+        Mirror(pRoot->left);
+        Mirror(pRoot->right);   
+    }
+};
