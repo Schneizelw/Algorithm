65ef4a127f2ae014eec8051c7e545ca6ca042548
Schneizel <744587537@qq.com>
2018-04-02 17:10:48 +0800

    Create 翻转单词顺序列.cpp

---
+++ CodingInterview/翻转单词顺序列.cpp
@@ -0,0 +1,30 @@
+/*
+时间限制：1秒 空间限制：32768K 热度指数：140961
+本题知识点： 字符串
+题目描述
+牛客最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。同事Cat对Fish写的内容颇感兴趣，
+有一天他向Fish借来翻看，但却读不懂它的意思。例如，“student. a am I”。后来才意识到，这家伙原来把句子单词的顺序翻转了，
+正确的句子应该是“I am a student.”。Cat对一一的翻转这些单词顺序可不在行，你能帮助他么？
+
+*/
+
+class Solution {
+public:
+    string ReverseSentence(string str) {
+        int len = str.length();
+        if(len<2) return str;
+        string res;
+        int cnt = 0;
+        for(int i=len-1;i>=0;--i){
+            if(str[i]==' '){
+                res += str.substr(i+1,cnt);
+                res += ' ';
+                cnt=0;
+            }else{
+                cnt++;
+            }
+        }
+        res += str.substr(0,cnt);
+        return res;
+    }
+};
