1c64c221d07947f368862998f96aec4bdd25bb34
Schneizel <744587537@qq.com>
2018-04-02 16:33:26 +0800

    Create 和为S的连续正数序列.cpp

---
+++ CodingInterview/和为S的连续正数序列.cpp
@@ -0,0 +1,34 @@
+/*
+时间限制：1秒 空间限制：32768K 热度指数：99637
+题目描述
+小明很喜欢数学,有一天他在做数学作业时,要求计算出9~16的和,他马上就写出了正确答案是100。但是他并不满足于此,他在想究竟有多少种连续的正数序列的和为100(至少包括两个数)。没多久,他就得到另一组连续正数和为100的序列:18,19,20,21,22。现在把问题交给你,你能不能也很快的找出所有和为S的连续正数序列? Good Luck!
+输出描述:
+输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序
+*/
+
+class Solution {
+public:
+    vector<vector<int> > FindContinuousSequence(int sum) {
+        vector<vector<int>> result;
+        if(sum<3) return result;
+        list<int> temp;
+        int tot=0;
+        int limit = sum/2+1;
+        for(int i=1;i<=limit;++i){
+            tot+=i;
+            temp.push_back(i);
+            while(tot>sum){
+                tot-=temp.front();
+                temp.pop_front();
+            }
+            if(tot==sum){
+                vector<int> tempVec;
+                for(auto bg = temp.begin();bg!=temp.end();++bg){
+                    tempVec.push_back(*bg);
+                }
+                result.emplace_back(tempVec);
+            }
+        }
+        return result;
+    }
+};
