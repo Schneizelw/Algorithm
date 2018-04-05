9da3b6119a2768eecaeed9c484ec78bc111471e1
Schneizel <744587537@qq.com>
2018-04-05 16:41:27 +0800

    Update 链表中环的入口结点.cpp

--- CodingInterview/链表中环的入口结点.cpp
+++ CodingInterview/链表中环的入口结点.cpp
@@ -35,3 +35,60 @@ public:
         
     }
 };
+
+
+
+/*
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+        val(x), next(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    ListNode* meetNode(ListNode* pHead)
+    {
+        if(!pHead) return nullptr;
+        ListNode* pSlow = pHead->next;
+        if(!pSlow) return nullptr;
+        ListNode* pFast = pSlow->next;
+        
+        while(pFast)
+        {
+            if(pFast==pSlow)
+                return pFast;
+            pSlow = pSlow->next;
+            pFast = pFast->next;
+            if(pFast){
+                pFast = pFast->next;
+            }
+        }
+        return nullptr;
+    }
+    ListNode* EntryNodeOfLoop(ListNode* pHead)
+    {
+        ListNode* pNode =  meetNode(pHead);
+        if(!pNode) return nullptr;
+        ListNode* curPtr =  pNode;
+        int cnt = 1;
+        while(curPtr->next!=pNode)
+        {
+            curPtr = curPtr->next;
+            cnt++;
+        }
+        ListNode* p1=pHead,*p2=pHead;
+        while(cnt--)
+        {
+            p2 = p2->next;
+        }
+        while(p1!=p2){
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return p1;
+        
+    }
+};
